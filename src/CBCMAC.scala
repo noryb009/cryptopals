c8b436ffce038cac763cea21d8c8615761f9a9f1
noryb009 <noryb009@gmail.com>
2016-11-21 23:07:54 -0500

    S7C49 Part 2

--- src/CBCMAC.scala
+++ src/CBCMAC.scala
@@ -9,16 +9,28 @@ object CBCMAC {
       (data, iv, mac)
     }
 
-    def server(key: String, req: Seq[Byte]): Option[KeyVal.Pairs] = {
-      val (message, iv, mac) = splitParts(req)
-      val exp = CBCMAC(message, key, Some(iv))
+    def splitPartsFixedIV(m: Seq[Byte]): (Seq[Byte], Seq[Byte]) =
+      m.splitAt(m.length - 16)
+
+    def serverInner(message: Seq[Byte], key: String, mac: Seq[Byte], iv: Option[Seq[Byte]]): Option[KeyVal.Pairs] = {
+      val exp = CBCMAC(message, key, iv)
       if(exp != mac)
         None
       else
         Some(KeyVal(Utils.binaryToString(message)))
     }
 
-    def clientReq(key: String, from: Int)(to: Int, amount: Int): Seq[Byte] = {
+    def server(key: String, req: Seq[Byte]): Option[KeyVal.Pairs] = {
+      val (message, iv, mac) = splitParts(req)
+      serverInner(message, key, mac, Some(iv))
+    }
+
+    def serverFixedIV(key: String, req: Seq[Byte]): Option[KeyVal.Pairs] = {
+      val (message, mac) = splitPartsFixedIV(req)
+      serverInner(message, key, mac, None)
+    }
+
+    def clientReq(key: String, from: Int)(to: Int, amount: BigInt): Seq[Byte] = {
       val iv = AES.randomBytes(16)
       val text = s"from=$from&to=$to&amount=$amount"
       val message = Utils.stringToBinary(text)
@@ -26,15 +38,23 @@ object CBCMAC {
       message ++ iv ++ mac
     }
 
+    def clientReqFixedIV(key: String, from: Int)(tx: Seq[(Int, BigInt)]): Seq[Byte] = {
+      val txstr = tx.map{case (to, amount) => s"$to:$amount"}.mkString(";")
+      val text = s"from=$from&tx_list=$txstr"
+      val message = Utils.stringToBinary(text)
+      val mac = CBCMAC(message, key)
+      message ++ mac
+    }
+
     /* The attacker owns both acc1 and acc2,
      * and victim's ID has at most the same number of digits in acc1.
      */
-    def forge(client: (Int, Int) => Seq[Byte], victim: Int, acc1: Int, acc2: Int): Seq[Byte] = {
+    def forge(client: (Int, BigInt) => Seq[Byte], victim: Int, acc1: Int, acc2: Int): Seq[Byte] = {
       val (data, iv, mac) = splitParts(client(acc2, 1000000))
       val text = Utils.binaryToString(data)
       val fromStr = Utils.stringToBinary(acc1.toString)
       val victimStrPrime = Utils.stringToBinary(victim.toString)
-      val victimStr = Seq.fill[Byte](fromStr.length - victimStrPrime.length)(0) ++ victimStrPrime
+      val victimStr = Seq.fill[Byte](fromStr.length - victimStrPrime.length)('0') ++ victimStrPrime
 
       val acc1Start = text.indexOf("from=") + 5
       val xor = Seq.fill[Byte](acc1Start)(0) ++ XOR.xor(victimStr, fromStr.toIndexedSeq) ++ Seq.fill[Byte](16 - acc1Start - victimStr.length)(0)
@@ -44,6 +64,31 @@ object CBCMAC {
       val newData = Utils.stringToBinary(newText)
 
       newData ++ newIV ++ mac
+    }
+
+    /* This assumes that each transaction fails independently.
+     *
+     * This also assumes the client can generate a request to transfer more
+     * than the amount in the account.
+     */
+    def forgeFixedIV(client: Seq[(Int, BigInt)] => Seq[Byte], captured: Seq[Byte], to: Int): Seq[Byte] = {
+      /* We have C|C|C, and mac = M
+       * and G|G|G, and mac = N
+       * We want C|C|C|G|G|G, mac = O
+       * We know N != O != M, but
+       * C|C|C|(G^M)|G|G, mac = T = N
+       *
+       * We can have G = "from=...&..." + "[tx_list=]##:to=amount"
+       * then we can convert G to garbage.
+       */
+      val (capMsg, capMac) = splitPartsFixedIV(captured)
+      val c = AES.padPKCS7(capMsg)
+
+      val gRes = client(Seq((1, BigInt(0)), (to, BigInt(1000000))))
+      val (g, gMac) = splitPartsFixedIV(gRes)
+      val (gStart, gEnd) = g.splitAt(16)
+      val gMod = XOR.xor(gStart, capMac.toIndexedSeq) ++ gEnd
+      c ++ gMod ++ gMac
     }
   }
 }
--- test/Set7.scala
+++ test/Set7.scala
@@ -3,7 +3,7 @@ import org.scalatest.FunSpec
 import scala.util.Random
 
 class Set7 extends FunSpec {
-  it("C49 With IV") {
+  it("C49 Part 1") {
     val attacker1 = Random.nextInt(Int.MaxValue) + 1
     val attacker2 = Random.nextInt(Int.MaxValue)
     val victim = Random.nextInt(attacker1) // Same or less digits
@@ -13,8 +13,25 @@ class Set7 extends FunSpec {
     val paramsOpt = CBCMAC.Forge.server(key, msg)
     assert(paramsOpt.isDefined)
     val params = paramsOpt.get
-    assert(params("from").contains(victim.toString))
+    assert(params("from").exists(_.toInt == victim))
     assert(params("to").contains(attacker2.toString))
     assert(params("amount").contains("1000000"))
+  }
+
+  it("C49 Part 2") {
+    val attacker = Random.nextInt(Int.MaxValue)
+    val victim = Random.nextInt(Int.MaxValue)
+    val key = AES.randomString(16)
+
+    val tx = Seq((Random.nextInt(Int.MaxValue), BigInt(Random.nextInt(Int.MaxValue))))
+    val captured = CBCMAC.Forge.clientReqFixedIV(key, victim)(tx)
+
+    val client = CBCMAC.Forge.clientReqFixedIV(key, attacker)_
+    val msg = CBCMAC.Forge.forgeFixedIV(client, captured, attacker)
+    val paramsOpt = CBCMAC.Forge.serverFixedIV(key, msg)
+    assert(paramsOpt.isDefined)
+    val params = paramsOpt.get
+    assert(params("from").contains(victim.toString))
+    assert(params("tx_list").get.split(";").contains(attacker.toString + ":1000000"))
   }
 }
